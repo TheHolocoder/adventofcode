bb56eeadcb5a9970b4c715182d4fa33e2556b35b
Loïc Boutter <loic.boutter@axiocode.com>
2024-12-03 07:34:06 +0100

    day 1 complete

--- day-1/a.ts
+++ day-1/a.ts
@@ -1,9 +1,33 @@
 import { runSolution } from '../utils.ts';
 
+const column1: number[] = [];
+const column2: number[] = [];
+const distances: number[] = [];
+
 /** provide your solution as the return of this function */
 export async function day1a(data: string[]) {
-  console.log(data);
-  return 0;
+    console.log(data);
+    data.forEach(line => {
+        if (line.length !== 0) {
+            // const r = line.match(/([0-9]) {3}([0-9])/);
+            const r = line.split('   ');
+            column1.push(Number(r[0]));
+            column2.push(Number(r[1]));
+        }
+    });
+
+    column1.sort();
+    column2.sort();
+    
+    if (column1.length !== column2.length) {
+        throw new Error("OUPSIE");
+    }
+
+    for (let i = 0; i < column1.length; ++i) {
+        distances.push(Math.abs(column2[i] - column1[i]));
+    }
+    
+    return distances.reduce((total, current) => total += current, 0);
 }
 
 await runSolution(day1a);
--- day-1/b.ts
+++ day-1/b.ts
@@ -1,9 +1,34 @@
 import { runSolution } from '../utils.ts';
 
+const column1: number[] = [];
+const column2: number[] = [];
+const similarities: number[] = [];
+
 /** provide your solution as the return of this function */
-export async function day1b(data: string[]) {
-  console.log(data);
-  return 0;
+export async function day1a(data: string[]) {
+    console.log(data);
+    data.forEach(line => {
+        if (line.length !== 0) {
+            const r = line.split('   ');
+            column1.push(Number(r[0]));
+            column2.push(Number(r[1]));
+        }
+    });
+
+    if (column1.length !== column2.length) {
+        throw new Error("OUPSIE");
+    }
+
+    for (let i = 0; i < column1.length; ++i) {
+        const num = column1[i];
+        const frequency = column2.filter(elem => elem === num).length;
+        similarities.push(num * frequency);
+    }
+    
+    console.log(similarities);
+    
+    return similarities.reduce((total, current) => total += current, 0);
 }
 
-await runSolution(day1b);
+await runSolution(day1a);
+
