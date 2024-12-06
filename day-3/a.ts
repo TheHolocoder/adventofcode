5c01def86d9d1c323e182f97d1dd46c339ae4991
Loïc Boutter <loic.boutter@axiocode.com>
2024-12-06 16:00:19 +0100

    day 3 complete

--- day-3/a.ts
+++ day-3/a.ts
@@ -2,8 +2,17 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day3a(data: string[]) {
-  console.log(data);
-  return 0;
+    let result = 0;
+    const rx = /mul\((\d{1,3}),(\d{1,3})\)/g;
+    for (const line of data) {
+        let match = rx.exec(line);
+        while (null !== match) {
+            result += Number(match[1]) * Number(match[2]);
+            match = rx.exec(line);
+        }
+    }
+
+    return result;
 }
 
 await runSolution(day3a);
--- day-3/b.ts
+++ day-3/b.ts
@@ -1,9 +1,34 @@
 import { runSolution } from '../utils.ts';
 
+// const mulIndex = 1;
+const number1Index = 2;
+const number2Index = 3;
+const doIndex = 4;
+const dontIndex = 5;
+
 /** provide your solution as the return of this function */
-export async function day3b(data: string[]) {
-  console.log(data);
-  return 0;
+export async function day3a(data: string[]) {
+    let result = 0;
+    let enabled = true;
+
+    const rx = /(mul)\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don't)\(\)/g;
+    for (const line of data) {
+        let match = rx.exec(line);
+        while (null !== match) {
+            if (undefined !== match[doIndex]) {
+                enabled = true;
+            } else if (undefined !== match[dontIndex]) {
+                enabled = false;
+            } else if (enabled) {
+                // mul
+                result += Number(match[number1Index]) * Number(match[number2Index]);
+            }
+            match = rx.exec(line);
+        }
+    }
+
+    return result;
 }
 
-await runSolution(day3b);
+await runSolution(day3a);
+
