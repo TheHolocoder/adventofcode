4807b743e054f2f41265b81e74909c027e235039
Loïc Boutter <loic.boutter@axiocode.com>
2024-12-13 16:38:49 +0100

    day 6 complete

--- day-6/a.ts
+++ day-6/a.ts
@@ -1,9 +1,88 @@
 import { runSolution } from '../utils.ts';
 
+type Position = {
+    x: number;
+    y: number;
+};
+
+const directions: Position[] = [
+    {x: 0, y: -1},
+    {x: 1, y: 0},
+    {x: 0, y: 1},
+    {x: -1, y: 0},
+];
+
+const display = (grid: string[][]): void => {
+    console.clear();
+    for(const line of grid) {
+        console.log(line.join(''));
+    }
+};
+
+const outOfBounds = (guard: Position, width: number, height: number): boolean => {
+    return guard.x < 0 || guard.x >= width || guard.y < 0 || guard.y >= height;
+};
+
 /** provide your solution as the return of this function */
 export async function day6a(data: string[]) {
-  console.log(data);
-  return 0;
+    console.log(data);
+    const grid: string[][] = [];
+    const visited: string[][] = [];
+    let guard: Position = {x: 0, y:0};
+    for (let i = 0; i < data.length; ++i) {
+        const line = data[i];
+        if (line.length) {
+            grid.push(line.split(''));
+            visited.push(line.split(''));
+            visited[i].fill('.');
+        }
+        const index = line.indexOf('^');
+        if (index !== -1) {
+            guard = {x: index, y: i};
+        }
+    }
+    const width = grid[0].length;
+    const height = grid.length;
+    let direction = 0;
+    visited[guard.x][guard.y] = 'X';
+
+    while (!outOfBounds(guard, width, height)) {
+        // diplay
+        display(grid);
+
+        // simulate
+        const v = directions[direction];
+        const newPos = {x: guard.x + v.x, y: guard.y + v.y};
+
+        if (outOfBounds(newPos, width, height)) {
+            break;
+        }
+
+        if (grid[newPos.y][newPos.x] === '#') {
+            direction = (direction + 1) % 4;
+        } else {
+            grid[newPos.y][newPos.x] = '^';
+            grid[guard.y][guard.x] = '.';
+            guard = newPos;
+            visited[newPos.x][newPos.y] = 'X';
+        }
+
+        // sleep
+        await new Promise(r => setTimeout(r, 100));
+    }
+
+    display(visited);
+
+    let count = 0;
+    for(const line of visited) {
+        for (const cell of line) {
+            if ('X' === cell) {
+                count++;
+            }
+        }
+    }
+
+    return count;
 }
 
 await runSolution(day6a);
--- day-6/b.ts
+++ day-6/b.ts
@@ -1,9 +1,119 @@
 import { runSolution } from '../utils.ts';
 
-/** provide your solution as the return of this function */
-export async function day6b(data: string[]) {
-  console.log(data);
-  return 0;
+type Position = {
+    x: number;
+    y: number;
+};
+
+type PositionPlus = Position & {
+    symbol: string;
 }
 
-await runSolution(day6b);
+const directions: PositionPlus[] = [
+    { x: 0, y: -1, symbol: '|' },
+    { x: 1, y: 0, symbol: '-' },
+    { x: 0, y: 1, symbol: '|' },
+    { x: -1, y: 0, symbol: '-' },
+];
+
+const display = (grid: string[][]): void => {
+    // console.clear();
+    for (const line of grid) {
+        console.log(line.join(''));
+    }
+};
+
+const outOfBounds = (guard: Position, width: number, height: number): boolean => {
+    return guard.x < 0 || guard.x >= width || guard.y < 0 || guard.y >= height;
+};
+
+const simulate = async (guard: Position, grid: string[][]): Promise<boolean> => {
+    const width = grid[0].length;
+    const height = grid.length;
+    let direction = 0;
+    let tourneEnRond = false;
+
+    grid[guard.y][guard.x] = '1';
+
+    while (!outOfBounds(guard, width, height)) {
+        // diplay
+        // display(grid);
+
+        // simulate
+        const v = directions[direction];
+        const newPos = { x: guard.x + v.x, y: guard.y + v.y };
+
+        if (outOfBounds(newPos, width, height)) {
+            break;
+        }
+
+        if (grid[newPos.y][newPos.x] === '#') {
+            direction = (direction + 1) % 4;
+        } else {
+            const newNumber = Number(grid[newPos.y][newPos.x]);
+            grid[newPos.y][newPos.x] = (newNumber + 1).toString();
+            if (newNumber + 1 > 3) {
+                tourneEnRond = true;
+                break;
+            }
+            guard = newPos;
+        }
+
+        // sleep
+        // await new Promise(r => setTimeout(r, 100));
+    }
+
+    if (tourneEnRond) {
+        display(grid);
+    }
+    // await new Promise(r => setTimeout(r, 1000));
+
+
+    return tourneEnRond;
+}
+
+/** provide your solution as the return of this function */
+export async function day6a(data: string[]) {
+    console.log(data);
+    const grid: string[][] = [];
+    let guard: Position = { x: 0, y: 0 };
+    for (let i = 0; i < data.length; ++i) {
+        grid[i] = [];
+        const line = data[i];
+        if (line.length) {
+            for (let j = 0; j < line.length; ++j) {
+                grid[i].push(line[j] === '#' ? '#' : '0');
+            }
+        }
+        const index = line.indexOf('^');
+        if (index !== -1) {
+            guard = { x: index, y: i };
+        }
+    }
+
+    let count = 0;
+    for (let y = 0; y < grid.length; ++y) {
+        for (let x = 0; x < grid[0].length; ++x) {
+            console.log(`Simulate with obstacle at ${x}:${y}, guard starting at ${guard.x}:${guard.y}`);
+            if (guard.x === x && guard.y === y) {
+                console.warn('This cell is occupied by the guard!');
+                continue;
+            }
+
+            if (grid[y][x] === '#') {
+                console.warn('This cell is blocked!');
+                continue;
+            }
+
+            const dgrid = grid.map(line => line.slice());
+            dgrid[y][x] = '#';
+            if (await simulate(guard, dgrid)) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+await runSolution(day6a);
